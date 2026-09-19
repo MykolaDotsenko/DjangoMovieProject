7cf9aa8f14b9438b19e13bddbabfed532bbbd890
Mykola D <130664123+MykolaDotsenko@users.noreply.github.com>
2026-09-19 06:13:25 +0300

    Refactor imdb/urls.py

--- imdb/urls.py
+++ imdb/urls.py
@@ -1,16 +1,31 @@
-from django.urls import path
-from .views import *
+from django.contrib.auth.views import LogoutView
+from django.urls import path, reverse_lazy
+
+from .views import (
+    GenreDetailView,
+    IndexView,
+    MovieDetailView,
+    MovieListView,
+    PersonDetailView,
+    PersonListView,
+    SignInView,
+    SignUpView,
+)
 
 app_name = "imdb"
 
 urlpatterns = [
-    path('', IndexView.as_view(), name="index"),
-    path('movie/list/', MovieListView.as_view(), name="movie-list"),
-    path('movie/<int:pk>/', MovieDetailView.as_view(), name="movie-detail"),
-    path('genre/<int:pk>/', GenreDetailView.as_view(), name="genre-detail"),
-    path('person/list/', PersonListView.as_view(), name="person-list"),
-    path('person/<int:pk>/', PersonDetailView.as_view(), name="person-detail"),
-    path('user/sign/out/', user_sign_out, name="user-sign-out"),
-    path('authorization/page/', AuthorizationView.as_view(), name="authorization-view"),
-    path('account/create/page/', CreateAccountView.as_view(), name="create-account-view"),
+    path("", IndexView.as_view(), name="index"),
+    path("movies/", MovieListView.as_view(), name="movie-list"),
+    path("movies/<int:pk>/", MovieDetailView.as_view(), name="movie-detail"),
+    path("genres/<int:pk>/", GenreDetailView.as_view(), name="genre-detail"),
+    path("people/", PersonListView.as_view(), name="person-list"),
+    path("people/<int:pk>/", PersonDetailView.as_view(), name="person-detail"),
+    path("auth/login/", SignInView.as_view(), name="login"),
+    path("auth/signup/", SignUpView.as_view(), name="signup"),
+    path(
+        "auth/logout/",
+        LogoutView.as_view(next_page=reverse_lazy("imdb:index")),
+        name="logout",
+    ),
 ]
