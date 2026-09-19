49e4993460dc2920d72b258405cd1cca0e24b529
Mykola D <130664123+MykolaDotsenko@users.noreply.github.com>
2026-09-19 06:13:23 +0300

    Refactor imdb/views.py

--- imdb/views.py
+++ imdb/views.py
@@ -1,85 +1,107 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView, ListView, DetailView
-from django.contrib.auth import logout, login, authenticate
-from django.shortcuts import redirect
 from django.contrib import messages
-from .models import *
-from .forms import *
+from django.contrib.auth import login
+from django.contrib.auth.views import LoginView
+from django.db.models import Count, Q
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, FormView, ListView, TemplateView
+
+from .forms import SignInForm, SignUpForm
+from .models import Genre, Movie, Participation, Person
+
 
 class IndexView(TemplateView):
     template_name = "imdb/index.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['movie_list'] = Movie.objects.order_by('?')[:6]
-        context['person_list'] = Person.objects.order_by('?')[:6]
-        
+        context["movie_list"] = Movie.objects.prefetch_related("genres").order_by(
+            "-rating", "title"
+        )[:6]
+        context["person_list"] = (
+            Person.objects.annotate(credit_count=Count("participation"))
+            .order_by("-credit_count", "last_name", "first_name")[:6]
+        )
         return context
 
 
 class MovieListView(ListView):
-    # model = Movie
-    queryset = Movie.objects.order_by("title")
+    model = Movie
+    paginate_by = 12
+
+    def get_queryset(self):
+        queryset = Movie.objects.prefetch_related("genres").order_by("title")
+        query = self.request.GET.get("q", "").strip()
+        if query:
+            queryset = queryset.filter(
+                Q(title__icontains=query) | Q(genres__name__icontains=query)
+            ).distinct()
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["query"] = self.request.GET.get("q", "").strip()
+        return context
+
 
 class MovieDetailView(DetailView):
     model = Movie
+
+    def get_queryset(self):
+        return Movie.objects.prefetch_related("genres")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        credits = list(
+            Participation.objects.filter(movie=self.object)
+            .select_related("person")
+            .order_by("person__last_name", "person__first_name")
+        )
+        context["actors"] = [credit.person for credit in credits if credit.role == "A"]
+        context["directors"] = [credit.person for credit in credits if credit.role == "D"]
+        return context
+
 
 class GenreDetailView(DetailView):
     model = Genre
     template_name = "imdb/genre.html"
 
+    def get_queryset(self):
+        return Genre.objects.prefetch_related("movies")
+
+
 class PersonListView(ListView):
     model = Person
     queryset = Person.objects.order_by("last_name", "first_name")
+    paginate_by = 18
+
 
 class PersonDetailView(DetailView):
     model = Person
- 
-
-def user_sign_out(request):
-    logout(request)
-    return redirect('imdb:index')
-
-class AuthorizationView(TemplateView):
-    template_name = 'imdb/authorization.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['form'] = AuthorizationForm()
+        context["credits"] = (
+            Participation.objects.filter(person=self.object)
+            .select_related("movie")
+            .order_by("-movie__release_date", "movie__title")
+        )
+        context["featured_trailer"] = self.object.get_featured_trailer()
         return context
-    
-    def post(self, request):
-        print(request.POST)
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        print(username, password)
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-        else:
-            messages.add_message(request, messages.INFO, "Wrong username or password")
-
-        return redirect('imdb:index')
 
 
-class CreateAccountView(TemplateView):
-    template_name = 'imdb/create_account.html'
+class SignInView(LoginView):
+    template_name = "imdb/authorization.html"
+    authentication_form = SignInForm
+    redirect_authenticated_user = True
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['form'] = CreateAccountForm()
-        return context
-    
-    def post(self, request):
-        username = request.POST.get('username')
-        password1 = request.POST.get('password1')
-        password2 = request.POST.get('password2')
 
-        if User.objects.filter(username=username).exists():
-            messages.add_message(request, messages.INFO, "Username already exists")
-            return redirect('imdb:create-account-view')
-        elif password1 != password2:
-            messages.add_message(request, messages.INFO, "Passwords dont match")
-            return redirect('imdb:create-account-view')           
-        else:
-            return redirect('imdb:index')
+class SignUpView(FormView):
+    template_name = "imdb/create_account.html"
+    form_class = SignUpForm
+    success_url = reverse_lazy("imdb:index")
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        messages.success(self.request, f"Welcome, {user.username}. Your account is ready.")
+        return super().form_valid(form)
